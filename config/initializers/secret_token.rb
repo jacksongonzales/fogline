a3557dec56321d9d76c5c770614a06b2a7f12707
Jackson Gonzales <jackson.gonzales@gmail.com>
2013-08-10 14:32:43 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fogline::Application.config.secret_key_base = '287dd89b2ac2f0b3e96134569ad68156172f53081af8b67734b461ce930e425bede95c8afec1710dfe592b60b9270f281c20c0833b3be5536ba3577e63ece58f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fogline::Application.config.session_store :cookie_store, key: '_fogline_session'
